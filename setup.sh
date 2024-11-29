aa8693da454c612c6b20d406fa5bd863340c317b
Maulik Madhavi <maulikmadhavi@gmail.com>
2024-11-29 17:25:51 +0800

    adjust setup.sh

--- setup.sh
+++ setup.sh
@@ -1,21 +1,29 @@
 # First copy current bashrc as backup abd cioy the things
 
-cp ~/.bashrc ~/.bashrc_bak
+# cp ~/.bashrc ~/.bashrc_bak
 mkdir -p ~/.config/nvim
-cp .config/nvim ~/.config/nvim
+cp -r .config/nvim/* ~/.config/nvim/.
 
 # === pixi
 curl -fsSL https://pixi.sh/install.sh | bash
 #pixi install
-pixi global install tmux yarn python=3.10 git nvim conda
 source ~/.bashrc
+pixi global install tmux yarn python=3.10 git nvim conda
+
 
 # === nvim
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
 source ~/.bashrc  # or ~/.zshrc, depending on your shell
-# nvm install --lts
-cp -r mydot_files/.config/nvim/ .config/.
-nvim --headless +PlugInstall +qall
+nvm install --lts
+if [ -f ~/.local/share/nvim/site/autoload/plug.vim ]; then
+    nvim --headless +PlugInstall +qall
+else
+  curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  nvim --headless +PlugInstall +qall
+fi
+
+
 
 # ===  Oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
