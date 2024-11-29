34f58f012b014570fa33169efee5399bb4858c15
Maulik Madhavi <maulikmadhavi@gmail.com>
2024-11-29 17:32:28 +0800

    bash,zsh script execute separate

--- setup.sh
+++ setup.sh
@@ -30,13 +30,4 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 cp .zshrc ~/.zshrc
 # cp .zshrc.pre-oh-my-zsh ~/.
 zsh
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-
-git clone https://github.com/wting/autojump.git ~/autojump && \
-  cd ~/autojump && \
-  ./install.py
-
-
-source ~/.zshrc
 
---
+++ setup_inside_zsh.sh
@@ -0,0 +1,9 @@
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+
+git clone https://github.com/wting/autojump.git ~/autojump && \
+  cd ~/autojump && \
+  ./install.py
+
+
+source ~/.zshrc
